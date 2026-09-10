6a1a47ac14a28a96808c3071b3777ff19ae3cb92
codegeass <denzelgatugu@outlook.com>
2026-09-10 07:44:20 +0300

    Add dart initial project configuration files and templates

---
+++ packages/dart/lib/src/kenya_locations.dart
@@ -0,0 +1,7 @@
+/// {@template kenya_locations}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class KenyaLocations {
+  /// {@macro kenya_locations}
+  const new();
+}
---
+++ packages/dart/test/src/kenya_locations_test.dart
@@ -0,0 +1,12 @@
+// Not required for test files
+// ignore_for_file: prefer_const_constructors
+import 'package:kenya_locations/kenya_locations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('KenyaLocations', () {
+    test('can be instantiated', () {
+      expect(KenyaLocations(), isNotNull);
+    });
+  });
+}
